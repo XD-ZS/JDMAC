ed60a4e4f251b3802ae961d4d7e58cf31514126f
xiaoJane <2427371513@qq.com>
2019-01-10 20:49:37 +0800

    Update corr.py

--- corr.py
+++ corr.py
@@ -1,15 +1,17 @@
-# Correlation Matrix Visualization 相关性矩阵可视化
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-train = pd.read_csv('../input/jinnan_round1_train_20181227.csv', encoding='gb18030')
-test = pd.read_csv('../input/jinnan_round1_testA_20181227.csv', encoding='gb18030')
+train = pd.read_csv('../data/jinnan_round1_train_20181227.csv', encoding='gb18030')
+test = pd.read_csv('../data/jinnan_round1_testA_20181227.csv', encoding='gb18030')
 cate_columns = [f for f in train.columns if f != '样本id']
 corr = train[cate_columns].corr()
 corr.fillna(0)
-corr.to_csv("../input/corr.csv", index=False, header=None)
-%matplotlib inline
-sns.set(rc={'figure.figsize':(50,50)})
+corr.to_csv("../data/corr.csv", index=False, header=None)
+f,fig=plt.subplots(figsize=(50,50),nrows=1)
+# sns.set(rc={'figure.figsize':(50,50)})
+fig.tick_params(axis='x',labelsize=20) # x轴
+fig.tick_params(axis='y',labelsize=20) # y轴
+sns.set(font_scale=1.5)
 g=sns.heatmap(corr, ax=fig,annot=True, fmt=".2f")
+f.savefig('../data/corr.jpg', dpi=100, bbox_inches='tight')
