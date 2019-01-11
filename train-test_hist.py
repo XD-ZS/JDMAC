39de091617cefd3b4360b0c4fd1512f1ad09b691
xiaoJane <2427371513@qq.com>
2019-01-11 22:47:55 +0800

    Update train-test_hist.py

--- train-test_hist.py
+++ train-test_hist.py
@@ -6,7 +6,12 @@ test = pd.read_csv('../data/jinnan_round1_testA_20181227.csv', encoding='gb18030
 f,(ax1,ax2)=plt.subplots(figsize=(100,100),nrows=1)
 # fig.tick_params(axis='x',labelsize=1000) # x轴
 # fig.tick_params(axis='y',labelsize=1000) # y轴,这里没用
-train.hist(ax=ax1,xlabelsize =50,ylabelsize =50)
+a=[]
+for f in train.columns:
+    if train[f].dtypes!='object':
+        a.append(f)
+b=train.hist(column=a,ax=ax1,xlabelsize =50,ylabelsize =5)
+[x.title.set_size(32) for x in b.ravel()]
 f.savefig("../data/train_hist.png")
 f,ax2=plt.subplots(figsize=(100,100),nrows=1)
 test.hist(ax=ax2,xlabelsize =50,ylabelsize =50)
