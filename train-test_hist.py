d7efa2901fe23cac932dc5990d59857279457d3d
xiaoJane <2427371513@qq.com>
2019-01-11 23:00:55 +0800

    Update train-test_hist.py

--- train-test_hist.py
+++ train-test_hist.py
@@ -3,16 +3,15 @@ import matplotlib.pyplot as plt
 
 train = pd.read_csv('../data/jinnan_round1_train_20181227.csv', encoding='gb18030')
 test = pd.read_csv('../data/jinnan_round1_testA_20181227.csv', encoding='gb18030')
-f,(ax1,ax2)=plt.subplots(figsize=(100,100),nrows=1)
+fig,(ax1,ax2)=plt.subplots(figsize=(100,100),nrows=2)
 # fig.tick_params(axis='x',labelsize=1000) # x轴
 # fig.tick_params(axis='y',labelsize=1000) # y轴,这里没用
 a=[]
 for f in train.columns:
     if train[f].dtypes!='object':
         a.append(f)
-b=train.hist(column=a,ax=ax1,xlabelsize =50,ylabelsize =5)
+b=train.hist(column=a,ax=ax,xlabelsize =50,ylabelsize =50)
 [x.title.set_size(32) for x in b.ravel()]
-f.savefig("../data/train_hist.png")
-f,ax2=plt.subplots(figsize=(100,100),nrows=1)
+fig.savefig("../data/train_hist.png")
 test.hist(ax=ax2,xlabelsize =50,ylabelsize =50)
-f.savefig("../data/test_hist.png")
+fig.savefig("../data/test_hist.png")
