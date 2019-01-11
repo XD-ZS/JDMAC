c555978ec02b5388c3341cde955bebe72628de14
xiaoJane <2427371513@qq.com>
2019-01-11 16:34:38 +0800

    Update train-test_hist.py

--- train-test_hist.py
+++ train-test_hist.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 train = pd.read_csv('../data/jinnan_round1_train_20181227.csv', encoding='gb18030')
 test = pd.read_csv('../data/jinnan_round1_testA_20181227.csv', encoding='gb18030')
-f,ax1=plt.subplots(figsize=(100,100),nrows=1)
+f,(ax1,ax2)=plt.subplots(figsize=(100,100),nrows=1)
 # fig.tick_params(axis='x',labelsize=1000) # x轴
 # fig.tick_params(axis='y',labelsize=1000) # y轴,这里没用
 train.hist(ax=ax1,xlabelsize =50,ylabelsize =50)
